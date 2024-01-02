a6e7c6583f1e60b8386c9358f0c3e260ed4ea624
wnstngs <64602598+wnstngs@users.noreply.github.com>
2024-01-02 15:41:45 +0200

    common: Asserition infra update

--- User/common/assert.hpp
+++ User/common/assert.hpp
@@ -62,18 +62,52 @@ private:
 }
 
 #ifndef NTECTIVE_ASSERTIONS_ACTIVE
-#ifdef NDEBUG
-#define NTECTIVE_ASSERTIONS_ACTIVE false  
-#else
-#define NTECTIVE_ASSERTIONS_ACTIVE true
-#endif
+    #ifdef NDEBUG
+        #define NTECTIVE_ASSERTIONS_ACTIVE false  
+    #else
+        #define NTECTIVE_ASSERTIONS_ACTIVE true
+    #endif
 #endif
 
-#define NTECTIVE_ASSERTION(Expression) (!NTECTIVE_ASSERTIONS_ACTIVE || bool(Expression))    \
-                                           ? void(0)                                        \
-                                           : (void)Common::Util::ASSERTION{                 \
-                                                 Common_Util_WSTRING(Expression),           \
-                                                __FILEW__,                                  \
-                                                 __FUNCTIONW__,                             \
-                                                 __LINE__                                   \
+#define NTECTIVE_ASSERTION(Expression)  (!NTECTIVE_ASSERTIONS_ACTIVE || bool(Expression))    \
+                                            ? void(0)                                        \
+                                            : (void)Common::Util::ASSERTION{                 \
+                                                  Common_Util_WSTRING(Expression),           \
+                                                  __FILEW__,                                 \
+                                                  __FUNCTIONW__,                             \
+                                                  __LINE__                                   \
+                                               }
+
+#define NTECTIVE_CHECK(Expression)      bool(Expression)                                            \
+                                           ? void(0)                                                \
+                                           : (void)Common::Util::ASSERTION{                         \
+                                                 Common_Util_WSTRING(Expression),                   \
+                                                 __FILEW__,                                         \
+                                                 __FUNCTIONW__,                                     \
+                                                 __LINE__,                                          \
+                                                 NTECTIVE_ASSERTIONS_ACTIVE                         \
+                                                     ? Common::Util::ASSERTION_EFFECT::Termination  \
+                                                     : Common::Util::ASSERTION_EFFECT::Log          \
                                              }
+
+#define NTECTIVE_CHECK_FAIL(Expression) (void)Common::Util::ASSERTION{                          \
+                                            L"[Always Fail]",                                   \
+                                            __FILEW__,                                          \
+                                            __FUNCTIONW__,                                      \
+                                            __LINE__,                                           \
+                                            NTECTIVE_ASSERTIONS_ACTIVE                          \
+                                                ? Common::Util::ASSERTION_EFFECT::Termination   \
+                                                : Common::Util::ASSERTION_EFFECT::Log           \
+                                        }
+
+#define ASSERTION_WATCH(...)                            Common_Util_DISPATCH_VA(Common_Util_AW_, __VA_ARGS__) 
+#define Common_Util_AW_(Expression)                     Watch((Expression), Common_Util_WSTRING(Expression)) 
+#define Common_Util_AW_1_(z)                            Common_Util_AW_(z) 
+#define Common_Util_AW_2_(z, a)                         Common_Util_AW_(z).Common_Util_AW_(a) 
+#define Common_Util_AW_3_(z, a, b)                      Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b) 
+#define Common_Util_AW_4_(z, a, b, c)                   Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b).Common_Util_AW_(c) 
+#define Common_Util_AW_5_(z, a, b, c, d)                Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b).Common_Util_AW_(c).Common_Util_AW_(d) 
+#define Common_Util_AW_6_(z, a, b, c, d, e)             Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b).Common_Util_AW_(c).Common_Util_AW_(d).Common_Util_AW_(e) 
+#define Common_Util_AW_7_(z, a, b, c, d, e, f)          Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b).Common_Util_AW_(c).Common_Util_AW_(d).Common_Util_AW_(e).Common_Util_AW_(f) 
+#define Common_Util_AW_8_(z, a, b, c, d, e, f, g)       Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b).Common_Util_AW_(c).Common_Util_AW_(d).Common_Util_AW_(e).Common_Util_AW_(f).Common_Util_AW_(g) 
+#define Common_Util_AW_9_(z, a, b, c, d, e, f, g, h)    Common_Util_AW_(z).Common_Util_AW_(a).Common_Util_AW_(b).Common_Util_AW_(c).Common_Util_AW_(d).Common_Util_AW_(e).Common_Util_AW_(f).Common_Util_AW_(g).Common_Util_AW_(h)
--- User/common/macros.h
+++ User/common/macros.h
@@ -1,7 +1,16 @@
-﻿#pragma once
+﻿/*!
+ *  @file       macros.h
+ *  @brief      Common utility macros.
+ */
+
+#pragma once
 
 #define Common_Util_CONCAT_(x, y)   x ## y
 #define Common_Util_CONCAT(x, y)    CONCAT_(x, y)
 #define Common_Util_STRING_(x)      #x
 #define Common_Util_STRING(x)       STRING_(x)
 #define Common_Util_WSTRING(x)      CONCAT(L, STRING(x))
+
+#define Common_Util_NUM_ARGS_(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, TOTAL, ...) TOTAL
+#define Common_Util_NUM_ARGS(...) Common_Util_NUM_ARGS_(__VA_ARGS__,12_,11_,10_,9_,8_,7_,6_,5_,4_,3_,2_,1_)
+#define Common_Util_DISPATCH_VA(macro, ...) Common_Util_CONCAT(macro,Common_Util_NUM_ARGS(__VA_ARGS__))(__VA_ARGS__)
